32b195091043f27a272b49fa52cdfc040a5ee030
dantesebastianbengtsson <75175398+dantesebastianbengtsson@users.noreply.github.com>
2024-12-20 12:04:19 +0100

    Add project files.

---
+++ Models/Habit.cs
@@ -0,0 +1,12 @@
+﻿namespace Segmentum.Models
+{
+    public class Habit
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Status { get; set; } // e.g., "Succeeded", "Skipped", "Failed"
+        public int SegmentId { get; set; }
+        public Segment? Segment { get; set; }
+    }
+}
